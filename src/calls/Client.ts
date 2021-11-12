dd9b915cf04fee6dab0cf36ad6fb57b1784ad83f
Ashutosh Sharma <iamashu2002@gmail.com>
2021-11-12 21:47:31 +0530

    Change the client base url

--- src/calls/Client.ts
+++ src/calls/Client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://stupefied-kalam-472325.netlify.app/",
+  baseURL: "https://stupefied-kalam-472325.netlify.app",
   timeout: 10000,
 });
-
 export default client;
